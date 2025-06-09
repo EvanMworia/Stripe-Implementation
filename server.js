8694bbaee937890400066028aa984450a6a7ec97
EvanMworia <evannji99@gmail.com>
2025-06-10 01:08:34 +0300

    Chore: Set up project files

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import route from './routes/checkoutSessionRoute.js';
 
 // 1. Load environment variables
 dotenv.config();
@@ -11,6 +12,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json()); // for parsing JSON requests
+
+app.use(route);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
